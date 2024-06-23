ee236fdafa7fc2cf9ca2d31be6a317097a8c6ca1
WanJiaZe <Wan@students.uni-marburg.de>
2024-06-23 23:26:03 +0200

    add assets and add background

--- class/sprite.js
+++ class/sprite.js
@@ -1,17 +1,37 @@
 class Sprite {
-  constructor({ position, imageSrc }) {
+  constructor({ position, imageSrc, frames = 1, scale = 1, idx = 0 }) {
     this.position = position;
-    this.height = 50;
-    this.width = 150;
     this.image = new Image();
     this.image.src = imageSrc;
+    this.scale = scale;
+    this.frames = frames;
+    this.idx = idx;
+    this.animationCounter = 0;
   }
 
   draw() {
-    c.drawImage(this.image, this.position.x, this.position.y);
+    c.drawImage(
+      this.image,
+      (this.image.width / this.frames) * this.idx,
+      0,
+      this.image.width / this.frames,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      (this.image.width / this.frames) * this.scale,
+      this.image.height * this.scale
+    );
   }
 
   update() {
     this.draw();
+    this.animationCounter++;
+    if (this.animationCounter % 10 === 0) {
+      if (this.idx < this.frames - 1) {
+        this.idx++;
+      } else {
+        this.idx = 0;
+      }
+    }
   }
 }
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ canvas.width = 1024;
 canvas.height = 576;
 let timer = 60;
 let timerId;
+let middle = canvas.width / 2;
+let center = canvas.height / 2;
 
 // background
 const background = new Sprite({
@@ -11,14 +13,35 @@ const background = new Sprite({
     x: 0,
     y: 0,
   },
-  imageSrc: "./assets/background.png",
+  imageSrc: "./assets/background/background.png",
   height: canvas.height,
   width: canvas.width,
 });
 
+const shop = new Sprite({
+  position: {
+    x: 640,
+    y: 128,
+  },
+  imageSrc: "./assets/background/shop.png",
+  height: 50,
+  width: 150,
+  scale: 2.75,
+  frames: 6,
+});
+
+const lamp = new Sprite({
+  position: {
+    x: 90,
+    y: center + 20,
+  },
+  imageSrc: "./assets/background/lamp.png",
+  height: 100,
+  width: 50,
+  scale: 3,
+});
+
 // players
-let middle = canvas.width / 2;
-let center = canvas.height / 2;
 
 const player = new Fighter({
   position: { x: middle - 200, y: center - 100 },
@@ -58,6 +81,8 @@ function animate() {
   window.requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   background.update();
+  shop.update();
+  lamp.update();
   player.update();
   enemy.update();
 
